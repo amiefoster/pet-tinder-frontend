dd2c064c84ca56163da2cdbbd38162741868f7b2
Amie Foster <amie.n.foster@gmail.com>
2021-10-27 13:01:34 -0600

    nothing

--- src/components/MatchingContainer.js
+++ src/components/MatchingContainer.js
@@ -1,57 +1,43 @@
-import MatchingCard from './MatchingCard';
-import {useEffect, useState} from 'react';
-
+import MatchingCard from "./MatchingCard";
+import { useEffect, useState } from "react";
 
 function MatchingContainer() {
-    const BASE_URL= 'http://localhost:9292/petprofiles'
-    const [petProfiles, setPetProfiles] = useState([])
+  const BASE_URL = "http://localhost:9292/petprofiles";
+  const [petProfiles, setPetProfiles] = useState([]);
 
-    // Get petProfiles
-    useEffect(() => {
-        fetch(BASE_URL)
-        .then(response => response.json())
-        .then(data => checkIfPetProfileExists(data))
-    }, [])
+  // Get petProfiles
+  useEffect(() => {
+    fetch(BASE_URL)
+      .then((response) => response.json())
+      .then((data) => checkIfPetProfileExists(data));
+  }, []);
 
-
-
-    function checkIfPetProfileExists(data){
-        if (!!data){
-            setPetProfiles(data)
-        }
+  function checkIfPetProfileExists(data) {
+    if (!!data) {
+      setPetProfiles(data);
     }
+  }
 
-    function renderPetProfiles (){
-        return petProfiles.map(
-            petProfile => {
-                return(
-                    <MatchingCard
-                    key={petProfile.id}
-                    petName={petProfile.name}
-                    species={petProfile.species}
-                    breed={petProfile.breed}
-                    petAge={petProfile.age}
-                    ownerHobby={petProfile.owner_hobby}
-                    ownerName={petProfile.owner_name}
-                    imageUrl={petProfile.image_url}
-                    profileLike={petProfile.profile_like}
-                    ownerAge={petProfile.owner_age}
-                    />
-                )
-            }
-        )
-    }
+  function renderPetProfiles() {
+    return petProfiles.map((petProfile) => {
+      return (
+        <MatchingCard
+          key={petProfile.id}
+          petName={petProfile.name}
+          species={petProfile.species}
+          breed={petProfile.breed}
+          petAge={petProfile.age}
+          ownerHobby={petProfile.owner_hobby}
+          ownerName={petProfile.owner_name}
+          imageUrl={petProfile.image_url}
+          profileLike={petProfile.profile_like}
+          ownerAge={petProfile.owner_age}
+        />
+      );
+    });
+  }
 
-
-
-
-
-    return(
-        <div>
-            {renderPetProfiles()}
-        </div>
-    )
-
+  return <div>{renderPetProfiles()}</div>;
 }
 
 export default MatchingContainer;

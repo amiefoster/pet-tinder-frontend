351b24db567a001b456c8eb3816fd69c5dd4c1d1
Neil B. Chanez Gracia <Chanez89@Hotmail.com>
2021-10-27 11:52:50 -0700

    fetched user profile

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,6 +1,59 @@
+import {useEffect, useState} from 'react';
+
 function UserProfile() {
+    
+    const USER_URL = 'http://localhost:9292/mainuser'
+    const [user, setUser] = useState([])
+
+
+    useEffect(() => {
+        fetch(USER_URL)
+        .then(response => response.json())
+        .then(data => checkIfUserExists(data))
+    }, [])
+
+
+    function checkIfUserExists(data){
+        if (!!data){
+            setUser(data)
+        }
+    }
+
+    let id = " ";
+    let petName = null;
+    let species = null;
+    let breed = null;
+    let age = null;
+    let owner_hobby= null;
+    let image_url= null;
+    let owner_name= null;
+    let owner_age= null;
+
+    function renderUser () {
+        return user.forEach(
+          singleUser => {
+                id = singleUser.id
+                petName = singleUser.name
+                species = singleUser.species
+                breed = singleUser.breed
+                age = singleUser.age
+                owner_hobby = singleUser.owner_hobby
+                image_url = singleUser.image_url
+                owner_name = singleUser.owner_name
+                owner_age = singleUser.owner_age
+                  console.log (id, "this is inside renderedUser ID 1")
+          }  
+
+        )
+    }
+    renderUser()
+
     return (
-        <div></div>
+        <div>
+            <div>
+                <p> {id}{petName}{species} </p>
+            </div>
+        </div>
         )
 }
 

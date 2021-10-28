32464273e26063e75940b74a0bf2de082bde4e9a
Neil B. Chanez Gracia <Chanez89@Hotmail.com>
2021-10-28 08:56:57 -0700

    began navbar/home styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,22 @@
 function Home() {
+    
+
+
     return (
-    <div className="home-page"></div>)
+        
+        <div className="home-page">
+            <h1 style={{fontSize: "8vw", color: "#FA8072", textShadow: "2px 2px 5px red", margin: "0", textAlign: "center" }}>DANDER
+            </h1>
+            {/* <p>sample text</p> */}
+            <div>
+                <h3>Find your Match!</h3>
+            </div>
+            <a href="/FindMatches">
+                <button id="linkToMatchingBttn">Start Matching</button>
+            </a>
+
+        </div>
+        )
 }
 
 export default Home;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,10 +6,14 @@ import {NavLink} from "react-router-dom"
 function Navbar() {
     return (
         <div>
-            <NavLink to="/">Home</NavLink>
-            <NavLink to="/FindMatches">Find Matches</NavLink>
-            <NavLink to="/Matches">Your Matches</NavLink>
-            <NavLink to="/UserProfile">Your Profile</NavLink>
+            <nav className = "navbar navbar-light nav-bar-styling rounded">
+                <div className="container">
+                    <NavLink to="/" className="text-muted no-underline" style={{textDecorationLine: "none" }}>🐾 Dander</NavLink>
+                    <NavLink to="/FindMatches" className="text-muted no-underline" style={{textDecorationLine: "none" }}>Find Matches</NavLink>
+                    <NavLink to="/Matches" className="text-muted no-underline" style={{textDecorationLine: "none" }}>Your Matches</NavLink>
+                    <NavLink to="/UserProfile" className="text-muted no-underline" style={{textDecorationLine: "none" }}>Your Profile</NavLink>
+                </div>
+            </nav>
         </div>
     )
 }

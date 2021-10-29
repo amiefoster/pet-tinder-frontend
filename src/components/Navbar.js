daf35cbf60f6d14d3a16884963d930801c1fe82c
Jacob <Jacobonasrallah@gmail.com>
2021-10-28 17:25:16 -0700

    styled home, nav, and expanded css sheet

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,13 @@
-import {NavLink} from "react-router-dom"
-
-
-
 
 function Navbar() {
     return (
         <div>
             <nav className = "navbar navbar-light nav-bar-styling rounded">
                 <div className="container">
-                    <NavLink to="/" className="text-muted no-underline" style={{textDecorationLine: "none" }}>🐾 Dander</NavLink>
-                    <NavLink to="/FindMatches" className="text-muted no-underline" style={{textDecorationLine: "none" }}>Find Matches</NavLink>
-                    <NavLink to="/Matches" className="text-muted no-underline" style={{textDecorationLine: "none" }}>Your Matches</NavLink>
-                    <NavLink to="/UserProfile" className="text-muted no-underline" style={{textDecorationLine: "none" }}>Your Profile</NavLink>
+                    <NavLink to="/" className="text-muted no-underline" style={{textDecorationLine: "none" }}><h1>🐾 Dander</h1></NavLink>
+                    <NavLink to="/FindMatches" className="text-muted no-underline" style={{textDecorationLine: "none" }}><h1>Find Matches</h1></NavLink>
+                    <NavLink to="/Matches" className="text-muted no-underline" style={{textDecorationLine: "none" }}><h1>View Matches</h1></NavLink>
+                    <NavLink to="/UserProfile" className="text-muted no-underline" style={{textDecorationLine: "none" }}><h1>Your Profile</h1></NavLink>
                 </div>
             </nav>
         </div>

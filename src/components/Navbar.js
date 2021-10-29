e1a63187f274871a5fb6330cb4f7487efeb4a0b4
Amie Foster <amie.n.foster@gmail.com>
2021-10-28 19:17:04 -0600

    added styling to logo in header

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,16 +4,16 @@ import logo from "../images/logo.png"
 function Navbar() {
     return (
         <div>
-            <nav className = "navbar navbar-light nav-bar-styling rounded">
+            <div className = "navbar navbar-light nav-bar-styling rounded">
                 <div className="container">
                     <img src={logo} className="logo"/>
-                    <NavLink to="/" className="text-muted no-underline" style={{textDecorationLine: "none" }}><h1>🐾 Home</h1></NavLink>
-                    <NavLink to="/FindMatches" className="text-muted no-underline" style={{textDecorationLine: "none" }}><h1>Find Matches</h1></NavLink>
-                    <NavLink to="/Matches" className="text-muted no-underline" style={{textDecorationLine: "none" }}><h1>View Matches</h1></NavLink>
-                    <NavLink to="/UserProfile" className="text-muted no-underline" style={{textDecorationLine: "none" }}><h1>Your Profile</h1></NavLink>
+                    <NavLink to="/" className="text-muted no-underline" style={{textDecorationLine: "none" }}><h3>🐾 Home</h3></NavLink>
+                    <NavLink to="/FindMatches" className="text-muted no-underline" style={{textDecorationLine: "none" }}><h3>Find Matches</h3></NavLink>
+                    <NavLink to="/Matches" className="text-muted no-underline" style={{textDecorationLine: "none" }}><h3>View Matches</h3></NavLink>
+                    <NavLink to="/UserProfile" className="text-muted no-underline" style={{textDecorationLine: "none" }}><h3>Your Profile</h3></NavLink>
 
                 </div>
-            </nav>
+            </div>
         </div>
     )
 }
